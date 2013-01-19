cbc0154e68e196e9691f480aaff5c097db9ec3d6
Victor de los Santos <vis.bcn@gmail>
2013-01-19 19:28:47 +0000

    Added PastMeetingImpl (methods inherited must be included)

---
+++ PastMeetingImpl.java
@@ -0,0 +1,15 @@
+import java.util.Calendar;
+import java.util.Set;
+
+
+public class PastMeetingImpl implements PastMeeting{
+	
+	private String notes;
+
+	public String getNotes() {
+		return notes;
+	}
+
+
+
+}
